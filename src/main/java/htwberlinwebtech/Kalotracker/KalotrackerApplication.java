e4e6bc25cbd17ccb3d2917c70a8b624e3bc454f0
LucasMader <lucas.mader@student.htw-berlin.de>
2022-05-15 14:59:00 +0200

    v1

---
+++ src/main/java/htwberlinwebtech/Kalotracker/KalotrackerApplication.java
@@ -0,0 +1,13 @@
+package htwberlinwebtech.Kalotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalotrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalotrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htwberlinwebtech/Kalotracker/KalotrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package htwberlinwebtech.Kalotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalotrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
